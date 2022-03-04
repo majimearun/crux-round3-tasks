f81fa328933a676017cd68d49a1df167103a6114
Arunachala A.M <arunachala0619@gmail.com>
2022-03-04 09:34:30 +0530

    removed unessacary datasets from update_data.py

--- update_data.py
+++ update_data.py
@@ -15,13 +15,6 @@ def get_time_series(url=base_url, save=False):
         (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame): 4 timeseries, 1 global (all 3 combined into 1) and 3 country wise (segregated into confirmed, recovered and deaths) respectively
     """
     ts_base_url = url + "/historical"
-    global_ts_url = ts_base_url + "/all?lastdays=all"
-
-    global_ts_update = requests.get(global_ts_url).json()
-
-    global_ts_df = pd.DataFrame(
-        global_ts_update, columns=global_ts_update.keys()
-    ).transpose()
 
     country_wise_ts_url = ts_base_url + "?lastdays=all"
 
@@ -30,7 +23,6 @@ def get_time_series(url=base_url, save=False):
     ts_index = [i["country"] for i in country_wise_ts]
 
     country_confirmed = [i["timeline"]["cases"] for i in country_wise_ts]
-    country_recovered = [i["timeline"]["recovered"] for i in country_wise_ts]
     country_deaths = [i["timeline"]["deaths"] for i in country_wise_ts]
 
     country_confirmed_df = pd.DataFrame(
@@ -47,33 +39,19 @@ def get_time_series(url=base_url, save=False):
         country_deaths_df.index, as_index=True
     ).sum()
 
-    country_recovered_df = pd.DataFrame(
-        country_recovered, columns=country_recovered[0].keys(), index=ts_index
-    )
-    country_recovered_df = country_recovered_df.groupby(
-        country_recovered_df.index, as_index=True
-    ).sum()
-
-    global_ts_df["category"] = global_ts_df.index
-    country_recovered_df["country"] = country_recovered_df.index
     country_deaths_df["country"] = country_deaths_df.index
     country_confirmed_df["country"] = country_confirmed_df.index
 
     if save:
-
-        global_ts_df.to_csv("./task1/data/global_ts.csv", index=False)
         country_confirmed_df.to_csv("./task1/data/country_confirmed.csv", index=False)
         country_deaths_df.to_csv("./task1/data/country_deaths.csv", index=False)
-        country_recovered_df.to_csv("./task1/data/country_recovered.csv", index=False)
 
-    return global_ts_df, country_confirmed_df, country_deaths_df, country_recovered_df
+    return country_confirmed_df, country_deaths_df
 
 
 if __name__ == "__main__":
 
     (
-        global_ts_df,
         country_confirmed_df,
         country_deaths_df,
-        country_recovered_df,
     ) = get_time_series(save=True)

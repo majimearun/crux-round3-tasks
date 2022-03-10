6e6e5a17f56ae973ac21a8cb5e7385261b63d07b
Arunachala A.M <arunachala0619@gmail.com>
2022-03-10 12:06:54 +0530

    updated requirements file and added new dataset
    Reason for poor performance of SARIMAX was high frequency data, and as I do not have a powerful computer, I will be doing the task with a new monthly dataset from kaggle

--- update_data.py
+++ update_data.py
@@ -7,13 +7,6 @@ base_url = "https://disease.sh/v3/covid-19"
 
 
 def get_time_series(url=base_url, save=False):
-    """
-    Args:
-        url (string, optional): common/home part of url used to derive other urls from which we get required information. Defaults to base_url
-        save (bool, optional): arg determing whether the content gathered/updated should be saved or not. Defaults to False
-    Returns:
-        (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame): 4 timeseries, 1 global (all 3 combined into 1) and 3 country wise (segregated into confirmed, recovered and deaths) respectively
-    """
     ts_base_url = url + "/historical"
 
     country_wise_ts_url = ts_base_url + "?lastdays=all"
@@ -23,7 +16,6 @@ def get_time_series(url=base_url, save=False):
     ts_index = [i["country"] for i in country_wise_ts]
 
     country_confirmed = [i["timeline"]["cases"] for i in country_wise_ts]
-    country_deaths = [i["timeline"]["deaths"] for i in country_wise_ts]
 
     country_confirmed_df = pd.DataFrame(
         country_confirmed, columns=country_confirmed[0].keys(), index=ts_index
@@ -32,26 +24,14 @@ def get_time_series(url=base_url, save=False):
         country_confirmed_df.index, as_index=True
     ).sum()
 
-    country_deaths_df = pd.DataFrame(
-        country_deaths, columns=country_deaths[0].keys(), index=ts_index
-    )
-    country_deaths_df = country_deaths_df.groupby(
-        country_deaths_df.index, as_index=True
-    ).sum()
-
-    country_deaths_df["country"] = country_deaths_df.index
     country_confirmed_df["country"] = country_confirmed_df.index
 
     if save:
         country_confirmed_df.to_csv("./task1/data/country_confirmed.csv", index=False)
-        country_deaths_df.to_csv("./task1/data/country_deaths.csv", index=False)
 
-    return country_confirmed_df, country_deaths_df
+    return country_confirmed_df
 
 
 if __name__ == "__main__":
 
-    (
-        country_confirmed_df,
-        country_deaths_df,
-    ) = get_time_series(save=True)
+    country_confirmed_df = get_time_series(save=True)
